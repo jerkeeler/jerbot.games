0289a86900c6f4b9db5ad54c62e634010200ee78
Jeremy Keeler <j@keeler.dev>
2024-02-10 20:15:32 -0500

    Icons, assets, and homepage

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let src: string;
+  export let alt: string;
+  export let href: string;
+</script>
+
+<a {href} class="ml-0 ml-12 mt-3 h-12 w-12 first:ml-0 hover:scale-125 transition-all">
+  <img {src} {alt} />
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Logo from '$lib/components/Logo.svelte';
+  import discord from '$lib/assets/icons/discord.svg';
+  import steam from '$lib/assets/icons/steam.svg';
+  import twitter from '$lib/assets/icons/x_twitter.svg';
+</script>
+
+<div class="container flex items-center justify-center bg-zinc-950">
+  <div class="flex flex-row justify-center text-white">
+    <Logo href="https://discord.com/users/181124824435916800" src={discord} alt="Discord logo" />
+    <Logo href="https://x.com/jerbot__" src={twitter} alt="Twitter logo" />
+    <Logo href="https://steamcommunity.com/profiles/76561198042875546/" src={steam} alt="Steam logo" />
+  </div>
+</div>
+
+<style>
+  .container {
+    min-height: 100vh;
+    min-width: 100%;
+  }
+</style>
